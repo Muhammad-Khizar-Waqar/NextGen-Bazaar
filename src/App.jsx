5a2133451c3ad1f1b6c54e9b23c4a5c7ff2fb2b2
Muhammad-Khizar-Waqar <muhammad.khizar.waqar@gmail.com>
2024-12-30 23:48:12 +0500

    creating header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import Header from "./components/header/Header";
+import { TopHeader } from "./components/header/TopHeader";
 
 const App = () => {
   return (
     <>
-      <h1>App</h1>
+      <TopHeader />
+      <Header />
     </>
   );
 };
